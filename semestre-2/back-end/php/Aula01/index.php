dfdb4f0a8348b65378c94ebc2c48000bb7f7d515
Espada-23 <felipevitorespada23@gmail.com>
2026-03-17 16:41:52 -0300

    fix: correct the directories

---
+++ semestre-2/back-end/php/Aula01/index.php
@@ -0,0 +1,15 @@
+<?php
+
+echo "Hello World!<br><br>";
+
+print "Meu primeiro arquivo PHP<br><br>";
+
+print "Criando variáveis:<br><br>";
+
+$num1 = 12;
+$num2 = 11;
+
+$soma = $num1 + $num2;
+
+echo "A soma de $num1 e $num2 é $soma";
+?>
