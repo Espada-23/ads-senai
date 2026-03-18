01fd6f30a9a97b5d7372eddc4bc3260cda4825e6
Espada-23 <felipevitorespada23@gmail.com>
2026-03-18 16:43:19 -0300

    feat: add multiplication table generator for a list of numbers

---
+++ back-end/php/aula02/tabuada.php
@@ -0,0 +1,14 @@
+<?php
+
+$numeros = [1, 2, 3, 4, 5];
+
+foreach($numeros as $numero){
+    
+    echo "Tabuado do $numero:<br>";
+    
+    for($i = 1; $i <= 10; $i++){
+        $tabuada = $numero * $i;
+        echo "$numero x $i = $tabuada<br>";
+    }
+};
+
