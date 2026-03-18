c2d944e439bea131ec43137dd79734ae6ae4c20f
FELIPE VITOR ESPADA <25254244@educ123.sp.senai.br>
2026-03-18 14:46:34 -0300

    feat: add script to count even and odd numbers in PHP

---
+++ back-end/php/aula02/par-ou-impar.php
@@ -0,0 +1,18 @@
+<?php
+
+$numeros = [1, 234, 38, 454, 567, 67, 79, 86, 59, 10];
+$pares = 0;
+$impares = 0;
+
+foreach($numeros as $numero) {
+    if($numero % 2 == 0){
+       $pares = $pares + 1;
+    }
+    else{
+        $impares = $impares + 1;
+    }
+}
+
+echo "Dos números listados, $pares são pares e $impares são impares.";
+
+?>
