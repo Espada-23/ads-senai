d2f4b81e499095a74ec6366598bb8078233e37fa
Espada-23 <felipevitorespada23@gmail.com>
2026-03-26 14:49:51 -0300

    chore: my first forms

---
+++ back-end/php/aula04/formulario/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Formulário de Contato</title>
+</head>
+
+<body>
+    <h1>Formulário de Contato</h1>
+    <form action="receba.php" method="post">
+        <input type="text" name="nome" placeholder="Nome">
+        <input type="text"  name="endereco" placeholder="Endereço">
+        <input type="email" name="email" placeholder="Email">
+    </form>
+</body>
+</html>
---
+++ back-end/php/aula04/formulario/receba.php
@@ -0,0 +1,10 @@
+<?php
+    $nome=$_POST['nome'];
+    $endereco=$_POST['endereco'];
+    $email=$_POST['email'];
+    $pais=$_POST['pais'];
+    
+    echo "Nome: " . $nome . "<br>";
+    echo "Endereço: " . $endereco . "<br>";
+    echo "Email: " . $email . "<br>";
+?>
