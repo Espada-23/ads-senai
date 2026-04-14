7135252819f673feb4059a71eb89d0e6a24cc790
Felipe Vitor Espada <felipevitorespada23@gmail.com>
2026-04-14 14:21:20 -0300

    chore: create a 'data base'

---
+++ projetos/construtech/estoque/data.php
@@ -0,0 +1,39 @@
+<?php
+
+$produto_estoque = [
+    [
+        'id' => 1,
+        'nome' => 'Cimento Cauê 50kg',
+        'categoria' => 'Bruto',
+        'quantidade' => 45,
+        'preco' => 38.90,
+    ],
+    [
+        'id' => 2,
+        'nome' => 'Martelo de Unha Thompson',
+        'categoria' => 'Ferramentas',
+        'quantidade' => 3,
+        'preco' => 45.00,
+    ],
+    [
+        'id' => 3,
+        'nome' => 'Porcelanato Delta 80x80',
+        'categoria' => 'Acabamento',
+        'quantidade' => 120,
+        'preco' => 89.90,
+    ],
+    [
+        'id' => 4,
+        'nome' => 'Tinta Acrílica Branca 18L',
+        'categoria' => 'Acabamento',
+        'quantidade' => 2,
+        'preco' => 249.00,
+    ],
+    [
+        'id' => 5,
+        'nome' => 'Vergalhão 3/8 pol 12m',
+        'categoria' => 'Bruto',
+        'quantidade' => 15,
+        'preco' => 62.10,
+    ],
+];
