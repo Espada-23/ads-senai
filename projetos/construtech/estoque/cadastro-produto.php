a6d9cd5090c82b904ce961228801ec6c3b762d98
Felipe Vitor Espada <felipevitorespada23@gmail.com>
2026-04-14 16:00:36 -0300

    rework: new data treatment

--- projetos/construtech/estoque/cadastro-produto.php
+++ projetos/construtech/estoque/cadastro-produto.php
@@ -3,30 +3,38 @@ include_once "init.php";
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-    $nome = $_POST['nome'] ?? '';
-    $categoria = $_POST['categoria'] ?? '';
-    $quantidade = $_POST['quantidade'] ?? '';
-    $preco = $_POST['preco'] ?? '';
+    $nome = trim($_POST['nome'] ?? '');
+    $categoria = trim($_POST['categoria'] ?? '');
+    $quantidade = trim($_POST['quantidade'] ?? '');
+    $preco = trim($_POST['preco'] ?? '');
 
-   if (!is_numeric($quantidade) || !is_numeric($preco)) {
-    echo "<script>
-            alert('Quantidade e preço devem ser números!');
-            window.history.back();
-          </script>";
-    exit;
-}
+    $preco = str_replace(',', '.', $preco);
 
-    $novoProduto = [
-        'nome' => $nome,
-        'categoria' => $categoria,
-        'quantidade' => $quantidade,
-        'preco' => $preco
-    ];
+    if($nome == '' || $quantidade == '' || $preco == '') {
+        echo "<script>
+                alert('Preencha todos os campos!');
+                window.history.back();
+            </script>";
+        exit;
+    }elseif (!is_numeric($quantidade) || !is_numeric($preco)) {
+        echo "<script>
+                alert('Quantidade e preço devem ser números!');
+                window.history.back();
+            </script>";
+        exit;
+    }
+    
+        $novoProduto = [
+            'nome' => $nome,
+            'categoria' => $categoria,
+            'quantidade' => $quantidade,
+            'preco' => $preco
+        ];
 
-    $_SESSION['produtos'][] = $novoProduto;
+        $_SESSION['produtos'][] = $novoProduto;
 
-    unset($_POST);
+        unset($_POST);
 
-    header("Location: estoque.php");
-    exit;
+        header("Location: estoque.php");
+        exit;
 }
